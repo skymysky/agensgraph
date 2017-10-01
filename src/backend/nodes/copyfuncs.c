7e08fd163dc9ae25aaa0ce34313dfd88584696e9
Junseok Yang <jsyang@bitnine.net>
2017-10-01 01:35:06 -0700

    feat: Apply Cypher expression to property index and constraint

--- src/backend/nodes/copyfuncs.c
+++ src/backend/nodes/copyfuncs.c
@@ -5527,7 +5527,7 @@ copyObject(const void *from)
 			retval = _copyCypherStmt(from);
 			break;
 		case T_CypherGenericExpr:
-			_copyCypherGenericExpr(from);
+			retval = _copyCypherGenericExpr(from);
 			break;
 		case T_CypherSubPattern:
 			retval = _copyCypherSubPattern(from);
